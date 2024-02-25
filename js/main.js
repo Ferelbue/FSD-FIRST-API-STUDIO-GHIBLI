410db24bca99aa6ac4dbdbf71d618a276dff9828
Fernando <ferelbue@gmail.com>
2024-02-25 17:19:17 +0100

    feat: modal with title and description from the selected  movie

--- js/main.js
+++ js/main.js
@@ -1,5 +1,20 @@
-const container1 = document.getElementById('container1')
-
+const container1 = document.getElementsByClassName('container1')
+let arrayCont1 = Array.from(container1)
+const container2 = document.getElementsByClassName('container2')
+let arrayCont2 = Array.from(container2)
+const container3 = document.getElementsByClassName('container3')
+let arrayCont3 = Array.from(container3)
+const container4 = document.getElementsByClassName('container4')
+let arrayCont4 = Array.from(container4)
+const container5 = document.getElementsByClassName('container5')
+let arrayCont5 = Array.from(container5)
+const container6 = document.getElementsByClassName('container6')
+let arrayCont6 = Array.from(container6)
+const container7 = document.getElementsByClassName('container7')
+let arrayCont7 = Array.from(container7)
+const container8 = document.getElementsByClassName('container8')
+let arrayCont8 = Array.from(container8)
+let info = "";
 let left = document.getElementsByClassName("carousel-control-prev")
 let arrayLeft = Array.from(left)
 
@@ -9,6 +24,12 @@ let datos = [];
 let primera = 0;
 let flag = 0;
 let vueltas = Math.floor(datos.length / 4)
+let imgScreen = [];
+
+
+
+
+
 
 fetch("https://ghibliapi.vercel.app/films")
   .then((response) => {
@@ -19,7 +40,7 @@ fetch("https://ghibliapi.vercel.app/films")
   .then((response) => {
     response.forEach(element => {
       datos.push(element);
-
+      console.log(datos)
     });
 
     for (let i = 1; i < 5; i++) {
@@ -27,6 +48,8 @@ fetch("https://ghibliapi.vercel.app/films")
       image.src = datos[i].image
       const text = document.getElementById(`titulo${i}`)
       text.textContent = datos[i].original_title
+      const text2 = document.getElementById(`titulo${i}1`)
+      text2.textContent = datos[i].title
     }
     primera = 1;
     vueltas = 5;
@@ -45,7 +68,7 @@ arrayLeft.map(
       }
 
       if (primera === 1) {
-       
+
         console.log(vueltas)
         for (let i = 5; i < 9; i++) {
 
@@ -54,6 +77,8 @@ arrayLeft.map(
           image.src = datos[vueltas].image
           const text = document.getElementById(`titulo${i}`)
           text.textContent = datos[vueltas].original_title
+          const text2 = document.getElementById(`titulo${i}1`)
+          text2.textContent = datos[i].title
           vueltas--;
         }
         primera--;
@@ -70,6 +95,8 @@ arrayLeft.map(
           image.src = datos[vueltas].image
           const text = document.getElementById(`titulo${i}`)
           text.textContent = datos[vueltas].original_title
+          const text2 = document.getElementById(`titulo${i}1`)
+          text2.textContent = datos[i].title
           vueltas--;
         }
         primera++;
@@ -78,7 +105,7 @@ arrayLeft.map(
 
 
       // vueltas -= vueltas - 4;
-      console.log(vueltas,"dasdasd")
+      console.log(vueltas, "dasdasd")
       if (vueltas <= 4) {
         vueltas = 17;
       }
@@ -102,7 +129,9 @@ arrayRigth.map(
           image.src = datos[vueltas].image
           const text = document.getElementById(`titulo${i}`)
           text.textContent = datos[vueltas].original_title
-          vueltas ++;
+          const text2 = document.getElementById(`titulo${i}1`)
+          text2.textContent = datos[i].title
+          vueltas++;
 
         }
         // vueltas += 4;
@@ -116,25 +145,176 @@ arrayRigth.map(
           image.src = datos[vueltas].image
           const text = document.getElementById(`titulo${i}`)
           text.textContent = datos[vueltas].original_title
-     
-          vueltas ++;
+          const text2 = document.getElementById(`titulo${i}1`)
+          text2.textContent = datos[i].title
+          vueltas++;
         }
         // vueltas += 4;
         primera++;
       }
-
-
-
-
-
-
 
       if (vueltas >= 21) {
         vueltas = 0;
       }
 
       console.log(vueltas)
+
     })
   })
+
+
+arrayCont1.map(
+  item => {
+    item.addEventListener("click", (evento) => {
+
+
+      for (let i = 1; i < datos.length; i++) {
+
+        if ((datos[i].title) === (arrayCont1[0].childNodes[1].childNodes[3].innerHTML)) {
+
+          document.getElementById("prueba").innerHTML = datos[i].description
+          document.getElementById("exampleModalLabel").innerHTML = datos[i].title
+          console.log(arrayCont1[0].childNodes[1].childNodes[3].innerHTML)
+          $('#exampleModal').modal('show');
+
+
+        }
+      }
+
+    })
+  })
+
+arrayCont2.map(
+  item => {
+    item.addEventListener("click", (evento) => {
+
+      for (let i = 1; i < datos.length; i++) {
+
+        if ((datos[i].title) === (arrayCont2[0].childNodes[1].childNodes[3].innerHTML)) {
+
+          document.getElementById("prueba").innerHTML = datos[i].description
+          document.getElementById("exampleModalLabel").innerHTML = datos[i].title
+          console.log(arrayCont1[0].childNodes[1].childNodes[3].innerHTML)
+          $('#exampleModal').modal('show');
+
+
+        }
+      }
+
+    })
+  })
+arrayCont3.map(
+  item => {
+    item.addEventListener("click", (evento) => {
+
+      for (let i = 1; i < datos.length; i++) {
+
+        if ((datos[i].title) === (arrayCont3[0].childNodes[1].childNodes[3].innerHTML)) {
+
+          document.getElementById("prueba").innerHTML = datos[i].description
+          document.getElementById("exampleModalLabel").innerHTML = datos[i].title
+          console.log(arrayCont1[0].childNodes[1].childNodes[3].innerHTML)
+          $('#exampleModal').modal('show');
+
+
+        }
+      }
+    })
+  })
+arrayCont4.map(
+  item => {
+    item.addEventListener("click", (evento) => {
+
+      for (let i = 1; i < datos.length; i++) {
+
+        if ((datos[i].title) === (arrayCont4[0].childNodes[1].childNodes[3].innerHTML)) {
+
+          document.getElementById("prueba").innerHTML = datos[i].description
+          document.getElementById("exampleModalLabel").innerHTML = datos[i].title
+          console.log(arrayCont1[0].childNodes[1].childNodes[3].innerHTML)
+          $('#exampleModal').modal('show');
+
+
+        }
+      }
+    })
+  })
+arrayCont5.map(
+  item => {
+    item.addEventListener("click", (evento) => {
+
+      for (let i = 1; i < datos.length; i++) {
+
+        if ((datos[i].title) === (arrayCont5[0].childNodes[1].childNodes[3].innerHTML)) {
+
+          document.getElementById("prueba").innerHTML = datos[i].description
+          document.getElementById("exampleModalLabel").innerHTML = datos[i].title
+          console.log(arrayCont1[0].childNodes[1].childNodes[3].innerHTML)
+          $('#exampleModal').modal('show');
+
+
+        }
+      }
+    })
+  })
+arrayCont6.map(
+  item => {
+    item.addEventListener("click", (evento) => {
+
+      for (let i = 1; i < datos.length; i++) {
+
+        if ((datos[i].title) === (arrayCont6[0].childNodes[1].childNodes[3].innerHTML)) {
+
+          document.getElementById("prueba").innerHTML = datos[i].description
+          document.getElementById("exampleModalLabel").innerHTML = datos[i].title
+          console.log(arrayCont1[0].childNodes[1].childNodes[3].innerHTML)
+          $('#exampleModal').modal('show');
+
+
+        }
+      }
+    })
+  })
+arrayCont7.map(
+  item => {
+    item.addEventListener("click", (evento) => {
+
+      for (let i = 1; i < datos.length; i++) {
+
+        if ((datos[i].title) === (arrayCont7[0].childNodes[1].childNodes[3].innerHTML)) {
+
+          document.getElementById("prueba").innerHTML = datos[i].description
+          document.getElementById("exampleModalLabel").innerHTML = datos[i].title
+          console.log(arrayCont1[0].childNodes[1].childNodes[3].innerHTML)
+          $('#exampleModal').modal('show');
+
+
+        }
+      }
+    })
+  })
+arrayCont8.map(
+  item => {
+    item.addEventListener("click", (evento) => {
+
+      for (let i = 1; i < datos.length; i++) {
+
+        if ((datos[i].title) === (arrayCont8[0].childNodes[1].childNodes[3].innerHTML)) {
+
+          document.getElementById("prueba").innerHTML = datos[i].description
+          document.getElementById("exampleModalLabel").innerHTML = datos[i].title
+          console.log(arrayCont1[0].childNodes[1].childNodes[3].innerHTML)
+          $('#exampleModal').modal('show');
+
+
+        }
+      }
+    })
+  })
+
+
+
+
+
 
 
